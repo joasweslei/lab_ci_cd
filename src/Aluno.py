794b622fe541c45903e385de408dad3fe969ed42
joasweslei <joasweslei@gmail.com>
2026-06-08 15:21:00 -0300

    feat(Aluno): adiciona a classe Aluno

---
+++ src/Aluno.py
@@ -0,0 +1,8 @@
+class Aluno:
+    def __init__(self, matricula, nome):
+        self.matricula = matricula
+        self.nome = nome
+
+    def calculaAprovacao(self, nota, faltas):
+        # Regra: nota mínima 7.0 e máximo 10 faltas
+        return nota >= 7.0 and faltas <= 10
